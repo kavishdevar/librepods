bfebd7f44e26065cf6a6315b8c8d7756a104af66
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-08-20 17:01:48 +0300

    build(wear): remove unused plugin from first-build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Minimal top-level build configuration for the autonomous Wear OS module.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.aboutLibraries) apply false
-//    alias(libs.plugins.hilt) apply false
 }
