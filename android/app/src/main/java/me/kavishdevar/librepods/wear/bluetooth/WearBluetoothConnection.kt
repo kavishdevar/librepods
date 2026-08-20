c568f893af77355c642b8a56257de88b73b4f430
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-08-20 21:32:56 +0300

    wear: expose AACP connect and reader through transport facade

--- android/app/src/main/java/me/kavishdevar/librepods/wear/bluetooth/WearBluetoothConnection.kt
+++ android/app/src/main/java/me/kavishdevar/librepods/wear/bluetooth/WearBluetoothConnection.kt
@@ -1,5 +1,6 @@
 package me.kavishdevar.librepods.wear.bluetooth
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import android.content.Context
 import kotlinx.coroutines.flow.StateFlow
@@ -18,6 +19,16 @@ class WearBluetoothConnection(context: Context) {
     }
 
     fun connectionState(): StateFlow<AirPodsConnectionSession.State>? = session?.state
+
+    /** Connect the AACP classic L2CAP channel on PSM 0x1001. */
+    @SuppressLint("MissingPermission")
+    fun connectAacp(device: BluetoothDevice) {
+        session?.connectAacp(device)
+            ?: error("AirPods connection session is not attached")
+    }
+
+    fun aacpInput() = session?.aacpInput
+        ?: error("AirPods AACP transport is not connected")
 
     fun close() {
         session?.close()
